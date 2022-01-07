5c33cf243c754f9a05ed763732a3bd350f2d99b5
Dhruv <dhruvsakariya2304@gmail.com>
2022-01-07 14:17:58 +0530

    Deploy

---
+++ src/data/index.js
@@ -0,0 +1,73 @@
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./blog-post.gif",
+    link: "https://blog-post-pink.vercel.app/",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./food-order.gif",
+    link: "https://reverent-visvesvaraya-a9699f.netlify.app",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./shopping-app.gif",
+    link: "https://expo.dev/@dhruvsakariya/akshar-enterprise-shope",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./make-meal-app.gif",
+    link: "https://expo.dev/@dhruvsakariya/meal-app-navigation",
+  },
+];
+
+export const skills = [
+  "Html",
+  "Css",
+  "Javascript",
+  "Python",
+  "ReactJs",
+  "Redux",
+  "NextJs",
+  "Sass",
+  "Bootstrap",
+  "WebPack",
+  "GraphQl",
+  "Apollo",
+  "React-Native",
+  "communication skill",
+  "Git",
+  "Github",
+  "Team Working",
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti saepe voluptas sed rerum quis? Quas dolores ipsam porro animi accusantium exercitationem! Deleniti, sint eius. Velit provident inventore officia asperiores hic!",
+    image:
+      "https://lh3.googleusercontent.com/j5jxenZpx9ZmJB-kO-tAvFYJlgTuHwX9ST0nf6ouPRe_til1bOSjdSCMsGnBK47CtStLKWu2qEr57W-r=w1080-h608-p-no-v0",
+    name: "Julie",
+    company: "Akshar Enterprise",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti saepe voluptas sed rerum quis? Quas dolores ipsam porro animi accusantium exercitationem! Deleniti, sint eius. Velit provident inventore officia asperiores hic!",
+    image:
+      "https://lh3.googleusercontent.com/j5jxenZpx9ZmJB-kO-tAvFYJlgTuHwX9ST0nf6ouPRe_til1bOSjdSCMsGnBK47CtStLKWu2qEr57W-r=w1080-h608-p-no-v0",
+    name: "Julie",
+    company: "Akshar Enterprise",
+  },
+];
